e7bc85b856a1afa8875f5a7a0fb55e615accf6b3
Brayan Hernandez <brayannhc07@gmail.com>
2024-10-23 22:31:32 -0600

    improving the ui

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -27,18 +27,18 @@ local use = require('packer').use
 use('wbthomason/packer.nvim')
 
 -- Monokai Pro theme.
-use {
-  "loctvl842/monokai-pro.nvim",
-  config = function()
-    require("monokai-pro").setup({
-        transparent_background = true,
-        terminal_colors = true,
-        devicons = true,
-        filter = "pro",
-      })
-    vim.cmd('colorscheme monokai-pro')
-  end
-}
+-- use {
+--   "loctvl842/monokai-pro.nvim",
+--   config = function()
+--     require("monokai-pro").setup({
+--         transparent_background = true,
+--         terminal_colors = true,
+--         devicons = true,
+--         filter = "spectrum",
+--       })
+--     vim.cmd('colorscheme monokai-pro')
+--   end
+-- }
 
 -- Ayu theme.
 -- use({
@@ -50,23 +50,30 @@ use {
 --   })
 
 -- One Dark theme.
--- use({
---     'jessarcher/onedark.nvim',
---     config = function()
---       vim.cmd('colorscheme onedark')
+use({
+    'jessarcher/onedark.nvim',
+    config = function()
+      vim.cmd('colorscheme onedark')
 
---       vim.api.nvim_set_hl(0, 'FloatBorder', {
---           fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
---           bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
---         })
+      vim.api.nvim_set_hl(0, 'FloatBorder', {
+          fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+          bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+        })
 
---       vim.api.nvim_set_hl(0, 'CursorLineBg', {
---           fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
---           bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
---         })
---       vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
---     end,
---   })
+      vim.api.nvim_set_hl(0, 'CursorLineBg', {
+          fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+          bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+        })
+      vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
+
+      vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+          fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+          bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+        })
+
+      vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
+    end,
+  })
 
 -- Commenting support.
 use('tpope/vim-commentary')
@@ -183,6 +190,43 @@ use({
       require('user/plugins/nvim-tree')
     end,
   })
+
+-- A Status line
+use({
+    'nvim-lualine/lualine.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('user/plugins/lualine')
+    end,
+  })
+
+-- Display buffers as tabs
+-- use({
+--     'akinsho/bufferline.nvim',
+--     requires = 'kyazdani42/nvim-web-devicons',
+--     after = 'monokai-pro.nvim',
+--     config = function()
+--       require('user/plugins/bufferline')
+--     end,
+--   })
+
+-- Display indentation lines.
+-- use({
+--     'lukas-reineke/indent-blankline.nvim',
+--     config = function()
+--       require('user/plugins/indent-blankline')
+--     end,
+--   })
+
+-- Add a dashboard
+use {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    requires = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('user/plugins/dashboard-nvim')
+    end,
+  }
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
