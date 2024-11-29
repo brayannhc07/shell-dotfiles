ccf4eeb649fb619eb4660ed22ec8cf3574d7d57b
Brayan Hernandez <brayannhc07@gmail.com>
2024-11-29 16:18:54 -0600

    hotfix: override monokai pro pallet colors

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -41,6 +41,17 @@ use {
           context_start_underline = false,
         },
       },
+      overridePalette = function(filter)
+        return {
+          accent1 = "#F97BB0",
+          accent2 = "#82E6FF",
+          accent3 = "#D7C781",
+          accent4 = "#75C2B3",
+          accent5 = "#5AD4E6",
+          accent6 = "#948AE3",
+          white = "#F7F1FF",
+        }
+      end
     })
     vim.cmd('colorscheme monokai-pro')
   end
